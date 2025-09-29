47d504a7da20465189bcb72c3da56d79556b260b
Carlos <chspv2007@gmail.com>
2025-09-29 15:42:04 -0300

    Documentação completa

--- main.py
+++ main.py
@@ -82,108 +82,165 @@ def validar_operacao(operacao) -> int:
         # Chamada da função error para qualquer outra resposta que seja dada
         error(operacao)
 
-
-
+# Função principal para o funcionamento da calculadora
 def em_funcionamento(operacao):
+    # Pedido para o usuário escolher qual operação matemática deseja
     print("Você escolheu a operação ", (operacao), ".")
+    # Pedido para o usuário escolher os 2 que quer utilizar para realizar a operação
     print("Digite os números:")
-    num1 = int(input("Número 1: "))
-    num2 = int(input("Número 2: "))
+    num1 = float(input("Número 1: "))
+    num2 = float(input("Número 2: "))
 
-    if (num1) == float or (num2) == float or (num1) == int or (num2) == int:
-        pass
-
+    # Condição para a operação ser soma
     if operacao == 1:
         print("Resultado:", soma(num1, num2))
+    # Condição para a operação ser subtração
     elif operacao == 2:
         print("Resultado:", subtracao(num1, num2))
+    # Condição para a operação ser multiplicação
     elif operacao == 3:
         print("Resultado:", multiplicacao(num1, num2))
+    # Condição para a operação ser divisão
     elif operacao == 4:
         print("Resultado:", divisao(num1, num2))
+    # Condição para qualquer outra resposta que seja dada
     else:
+        # Indicador de erro
         print("Operação inválida.")
+        # Indicador de loop para o usuário tentar novamente
         em_funcionamento(operacao)
+    # Chamada da função dnv caso o usuário queira tentar de novo
     dnv()
 
-
-
+# Função que transforma Celsius em Fahrenheit
 def C_F():
+    # Pedido de graus Celsius
     graus = float(input("Digite a temperatura em Celsius: "))
+    # Cálculo conversor
     Fahrenheit = (graus * 1.8) + 32
+    # Código para retornar a resposta com o texto descritivo
     return print(graus, "Graus Celsius em Fahrenheit é: ", Fahrenheit)
-    
+
+# Função que transforma Fahrenheit em Celsius
 def F_C():
+    # Pedido de graus Fahrenheit
     graus = float(input("Digite a temperatura em Fahrenheit: "))
+    # Cálculo conversor
     Celsius = (graus - 32) * 5 / 9
+    # Código para retornar a resposta com o texto descritivo
     return print(graus, "Graus Fahrenheit em Celsius: ", Celsius)
 
+# Função para saída do conversor
 def sair_conversor():
+    # Indicador de saída do conversor
     print("Saindo do conversor de temperatura. Até mais!")
+    # --FIM DO PROGRAMA--
     exit()
 
+# Função principal para o funcionamento do conversor de temperatura
 def conversor_temperatura():
+    # Bem vindas ao conversor de temperatura
     print("Bem Vindo ao Conversor de Temperatura!!!!")
+    # Opções possíveis de escolha para o usuário
     print("1. Celsius para Fahrenheit")
     print("2. Fahrenheit para Celsius")
     print("3. Sair")
+    # Decisão do usuário de qual função deseja utilizar
     operacao = int(input("Escolha a opção: "))
+    # Condição necessária para converter Celsius para Fahrenheit
     if operacao == 1:
+        # Chamada da função de Celsius para Fahrenheit
         C_F()
+        # Chamada da função pedido_conversor caso o usuário deseje utilizar o conversor novamente
         pedido_conversor()
+    # Condição necessária para converter Fahrenheit para Celsius
     elif operacao == 2:
+        # Chamada da função de Fahrenheit para Celsius
         F_C()
+        # Chamada da função pedido_conversor caso o usuário deseje utilizar o conversor novamente
         pedido_conversor()
+    # Condição necessária para o usuário sair do pedido_conversor
     elif operacao == 3:
+        # Chamada da função que finaliza o programa
         sair_conversor()
+    # Condição para qualquer outra resposta que seja dada
     else:
+        # Indicador de resposta inválida
         print("Opção inválida. Tente novamente.")
+        # Chamada da função conversor_temperatura para o usuário tentar novamente
         conversor_temperatura()
 
+# Função de repetição da calculadora
 def dnv():
-    print("Gostaria de realizar outra operação? (s/n)")
+    # Pergunta se outra operação será realizada
+    print("Gostaria de realizar outra operação?")
+    # Resposta do usuário
     resposta = input()
-    if resposta == 's' or resposta == 'S':
+    # Condições para validar a resposta que abrange várias possibilidades sinônimas ou refentes a sim
+    if resposta == 's' or resposta == 'S' or resposta == 'sim' or resposta == 'Sim' or resposta == 'SIM':
+        # Opções possíveis de escolha para o usuário
         print("1. Soma")
         print("2. Subtração")
         print("3. Multiplicação")
         print("4. Divisão")
         print("5. Sair")
+        # Decisão do usuário de qual função deseja utilizar
         operacao = int(input("Escolha a operação: "))
-        if operacao not in [5,4,3,2,1]:
+        # Condição para caso a decisão do usuário não seja válida
+        if operacao not in [5, 4, 3, 2, 1]:
+            # Indicador que a resposta do usuário não foi aceita
             print("Opção inválida. Tente novamente.")
+            # Chamada da função dnv para repetir até o usuário escolher uma resposta válida
             dnv()
-        elif operacao in [5,4,3,2,1]:
-            operacao = int(operacao)
-            em_funcionamento(operacao)
-            # validar_operacao(operacao)
-    elif resposta == 'n' or resposta == 'N':
+        # Condição para casa a decisão do usuário seja válida
+        elif operacao in [5, 4, 3, 2, 1]:
+            # Chamada da função validar_operacao que cria para qual caminho o usuário seguirá dependendo de sua resposta
+            validar_operacao(operacao)
+    # Condições para validar a resposta que abrange várias possibilidades sinônimas ou refentes a não
+    elif resposta == 'n' or resposta == 'N' or resposta == 'não' or resposta == 'Não' or resposta == 'NAO' or resposta == 'nao' or resposta == 'NÃO' or resposta == 'Nao':
+        # Chamada da função pedido_conversor para caso o usuário não queira utiliar mais a calculadora
         pedido_conversor()
+    # Condição para qualquer outra resposta que seja dada
     else:
-        print("Resposta inválida. Tente 's' e 'n'.")
+        # Indicador de resposta inválida
+        print("Resposta inválida. Tente 'sim' ou 'não'.")
+        # Chamada da função dnv para o usuário tentar de novo
         dnv()
 
+# Função error que é ativada caso o usuário escolha sair da calculadora ou dê uma resposta inválida
 def error(operacao):
-
+    # Condição necessária para sair da calculadora
     if operacao == 5:
+        # Indicador de saída da calculadora
         print("Saindo da calculadora.")
+        # Chamada da função pedido_conversor para perguntar o usuário se ele gostaria de usar o conversor de temperatura
         pedido_conversor()
+    # Condição necessária para ativar a calculadora logo após o usuário dar uma resposta inválida
     elif operacao in [1, 2, 3, 4]:
+        # Chamada da função em_funcionamento que ativa a calculadora utilizando a resposta dada do usuário
         em_funcionamento(int(operacao))
+    # Condição para qualquer outra resposta que seja dada
     elif operacao not in [1, 2, 3, 4, 5]:
+        # Indicador de resposta inválida
         print("Opção inválida. Tente novamente.")
+        # Decisão do usuário de qual função deseja utilizar
         a = int(input("Escolha a operação: "))
+        # Chamada da função error para dar a segunda chance do usuário acertar as respostas válidas
         error(a)
 
+# --COMEÇO DO PROGRAMA--
+# Bem vindas ao conversor de temperatura
 print("Bem Vindo à Calculadora Simples!!!!")
 
+# Opções possíveis de escolha para o usuário
 print("1. Soma")
 print("2. Subtração")
 print("3. Multiplicação")
 print("4. Divisão")
 print("5. Sair")
-
+# Decisão do usuário de qual função deseja utilizar
 operacao = int(input("Escolha a operação: "))
+# Chamada da função validar_operacao utilizando a resposta operacao dada pelo o usuário
 validar_operacao(operacao)
 
 # Fim do código.
